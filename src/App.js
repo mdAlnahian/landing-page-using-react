3a6177a3234b8b223d01929265e6ad0f02812626
mdAlnahian <muhammedalnahian@gmail.com>
2023-03-09 23:17:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AboutUs from './components/AboutUs/AboutUs';
+import Chef from './components/Chef/Chef';
+import FindUs from './components/FindUs/FindUs';
+import Footer from './components/Footer/Footer';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
+import Laurels from './components/Laurels/Laurels';
+import Navbar from './components/Navbar/Navbar';
+import SpecialMenu from './components/SpecialMenu/SpecialMenu';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Header></Header>
+      <AboutUs></AboutUs>
+      <SpecialMenu></SpecialMenu>
+      <Chef></Chef>
+      <Intro></Intro>
+      <Laurels></Laurels>
+      <Gallery></Gallery>
+      <FindUs></FindUs>
+      <Footer></Footer>
     </div>
   );
-}
+};
 
 export default App;
+
+
